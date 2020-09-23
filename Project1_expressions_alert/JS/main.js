93681b4417a2ca6917e331b11bfa83d6dce7b568
wilkmi24 <70912258+wilkmi24@users.noreply.github.com>
2020-09-22 21:03:57 -0700

    submitting assignment Project1_expressions_alert git status git status ::Q ls --help

--- Project1_expressions_alert/JS/main.js
+++ Project1_expressions_alert/JS/main.js
@@ -1,4 +1,17 @@
-var blues="I have the blues";
-var blues=blues.fontcolor("blue"); 
-document.write(blues);
-document.write(3+3);
+window.alert("Hello, World!")// An alert box will pop up at the opening of the webpage 
+ var A="This is a string";// difned the variable "A" 
+ document.write(A);// printed the varable "A" which states "This is a string"
+
+ document.write("   Mom told Sammy, \"Stop Barking!\".")//included quotation marks in my string text
+
+ document.write("  \"Jack and Jill went up the hill,"// combined to strings with quotations marks
+ +" to fetch a pail of water.\"   ")
+
+ var B="Concatenated" + " String"//defined variable B with two combined strings
+ document.write(B)//printed variable "B"
+ var Family= "The Smiths", dad="John", mom="Jane", sister="Jenny", brother="Danny";
+ document.write(mom)//defined multible variables
+var blues="I have the blues";//Here I'm defining the variable "blues"
+var blues=blues.fontcolor("blue"); //Here i'm changing th color for the font for the variable "blues"
+document.write(blues);//I'm telling the program to print "blues" 
+document.write(3+3);//telling the program to print the sum of 3+3
