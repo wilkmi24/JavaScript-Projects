c152e2059797411ca67d422a31d869d0483d1630
wilkmi24 <70912258+wilkmi24@users.noreply.github.com>
2020-09-28 12:59:42 -0700

    uploading project 4 to GitHub

--- Project4_dictionaries/main.js
+++ Project4_dictionaries/main.js
@@ -1,10 +1,11 @@
-function my_Dictionary() {
-    var vehicle = {
-        type = "Car",
-        make = "Subaru",
-        model = "crosstrek",
-        year = 2018,
-        transmission = "manual"
+function my_Dictionary() {//Named the function to be called in html
+    var vehicle = {//adding multiple descriptions to dicitonary entry "vehicle"
+        type :"Car",
+        make : "Subaru",
+        model : "crosstrek",
+        year : 2018,
+        transmission : "manual"
     };
+    delete vehicle.model;//removed/deleted an element fromt the dictionary
     document.getElementById("dictionary").innerHTML = vehicle.model;
 }
